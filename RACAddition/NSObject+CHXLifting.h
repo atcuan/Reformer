eb9583fb0791f40427a3c75241b409769e25bbad
Moch Xiao <cuzv@qq.com>
2015-08-26 17:34:32 +0800

    rac_lift

---
+++ RACAddition/NSObject+CHXLifting.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+CHXLifting.h
+//  Haioo
+//
+//  Created by Moch Xiao on 8/26/15.
+//  Copyright (c) 2015 Haioo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (CHXLifting)
+
+@end
---
+++ RACAddition/NSObject+CHXLifting.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+CHXLifting.m
+//  Haioo
+//
+//  Created by Moch Xiao on 8/26/15.
+//  Copyright (c) 2015 Haioo. All rights reserved.
+//
+
+#import "NSObject+CHXLifting.h"
+
+@implementation NSObject (CHXLifting)
+
+@end
